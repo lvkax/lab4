8a8acab1c596831f03af5861ccc49b9787772e6e
lvkax <sasha.livak22@gmail.com>
2024-09-24 12:50:19 +0300

    Добавьте файлы проекта.

---
+++ lab4/fMain.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace lab4
+{
+    partial class fMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fMain));
+            toolStrip1 = new ToolStrip();
+            btnAdd = new ToolStripButton();
+            btnEdit = new ToolStripButton();
+            tsSeparator1 = new ToolStripSeparator();
+            btnClear = new ToolStripButton();
+            btnDelete = new ToolStripButton();
+            tsSeparator2 = new ToolStripSeparator();
+            btnExit = new ToolStripButton();
+            bindScrPhones = new BindingSource(components);
+            gvPhones = new DataGridView();
+            toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)bindScrPhones).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)gvPhones).BeginInit();
+            SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            toolStrip1.BackColor = SystemColors.ControlLightLight;
+            toolStrip1.ImageScalingSize = new Size(20, 20);
+            toolStrip1.Items.AddRange(new ToolStripItem[] { btnAdd, btnEdit, tsSeparator1, btnClear, btnDelete, tsSeparator2, btnExit });
+            toolStrip1.Location = new Point(0, 0);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Size = new Size(836, 27);
+            toolStrip1.TabIndex = 0;
+            toolStrip1.Text = "toolStrip1";
+            // 
+            // btnAdd
+            // 
+            btnAdd.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            btnAdd.Image = (Image)resources.GetObject("btnAdd.Image");
+            btnAdd.ImageTransparentColor = Color.Magenta;
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(29, 24);
+            btnAdd.Text = "Додати запис про телефон";
+            // 
+            // btnEdit
+            // 
+            btnEdit.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            btnEdit.Image = (Image)resources.GetObject("btnEdit.Image");
+            btnEdit.ImageTransparentColor = Color.Magenta;
+            btnEdit.Name = "btnEdit";
+            btnEdit.Size = new Size(29, 24);
+            btnEdit.Text = "Редагувати запис";
+            // 
+            // tsSeparator1
+            // 
+            tsSeparator1.Name = "tsSeparator1";
+            tsSeparator1.Size = new Size(6, 27);
+            // 
+            // btnClear
+            // 
+            btnClear.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            btnClear.Image = (Image)resources.GetObject("btnClear.Image");
+            btnClear.ImageTransparentColor = Color.Magenta;
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(29, 24);
+            btnClear.Text = "Очистити дані";
+            btnClear.Click += toolStripButton3_Click;
+            // 
+            // btnDelete
+            // 
+            btnDelete.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            btnDelete.Image = (Image)resources.GetObject("btnDelete.Image");
+            btnDelete.ImageTransparentColor = Color.Magenta;
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(29, 24);
+            btnDelete.Text = "Видалити запис";
+            // 
+            // tsSeparator2
+            // 
+            tsSeparator2.Name = "tsSeparator2";
+            tsSeparator2.Size = new Size(6, 27);
+            // 
+            // btnExit
+            // 
+            btnExit.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            btnExit.Image = (Image)resources.GetObject("btnExit.Image");
+            btnExit.ImageTransparentColor = Color.Magenta;
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(29, 24);
+            btnExit.Text = "Вийти з програми ";
+            // 
+            // bindScrPhones
+            // 
+            bindScrPhones.CurrentChanged += bindScrPhones_CurrentChanged;
+            // 
+            // gvPhones
+            // 
+            gvPhones.AllowUserToAddRows = false;
+            gvPhones.AllowUserToDeleteRows = false;
+            gvPhones.AutoGenerateColumns = false;
+            gvPhones.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            gvPhones.DataSource = bindScrPhones;
+            gvPhones.Dock = DockStyle.Fill;
+            gvPhones.Location = new Point(0, 27);
+            gvPhones.Name = "gvPhones";
+            gvPhones.ReadOnly = true;
+            gvPhones.RowHeadersWidth = 51;
+            gvPhones.RowTemplate.Height = 29;
+            gvPhones.Size = new Size(836, 423);
+            gvPhones.TabIndex = 1;
+            // 
+            // fMain
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(836, 450);
+            Controls.Add(gvPhones);
+            Controls.Add(toolStrip1);
+            Name = "fMain";
+            Text = "Лабораторна робота №4";
+            Load += fMain_Load;
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)bindScrPhones).EndInit();
+            ((System.ComponentModel.ISupportInitialize)gvPhones).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ToolStrip toolStrip1;
+        private ToolStripButton btnAdd;
+        private ToolStripButton btnEdit;
+        private ToolStripSeparator tsSeparator1;
+        private ToolStripButton btnClear;
+        private ToolStripButton btnDelete;
+        private ToolStripSeparator tsSeparator2;
+        private ToolStripButton btnExit;
+        private BindingSource bindScrPhones;
+        private DataGridView gvPhones;
+    }
+}
---
+++ lab4/fMain.cs
@@ -0,0 +1,30 @@
+namespace lab4
+{
+    public partial class fMain : Form
+    {
+        public fMain()
+        {
+            InitializeComponent();
+        }
+        private void fMain_Resize(object sender, EventArgs e)
+        {
+            int buttonSize = 5 * btnAdd.Width + 2 * tsSeparator1.Width + 30;
+            btnExit.Margin = new Padding(Width - buttonSize, 0, 0, 0);
+        }
+
+        private void fMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bindScrPhones_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
